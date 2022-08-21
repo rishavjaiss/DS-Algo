bc22409ef0ac0798f95b7710e3c462abc5290887
Rishav Jaiswal <rishavjaiss14@gmail.com>
2022-08-21 22:05:50 +0530

    Create doublylinkedlist.js

---
+++ doublylinkedlist.js
@@ -0,0 +1,82 @@
+class Node {
+    constructor(value){
+        this.value = value
+        this.next = null
+        this.prev = null
+    }
+}
+
+class LinkedList {
+    constructor(value){
+        this.head = {
+            value: value,
+            next: null,
+            prev: null
+        }
+        this.tail = this.head
+        this.length = 1
+    }
+
+    printList(){
+        const array = []
+        let currentNode = this.head
+        while(currentNode !== null){
+            array.push(currentNode.value)
+            currentNode = currentNode.next
+        }
+        console.log(array)
+    }
+
+    append(value){
+        const newNode = new Node(value)
+        newNode.prev = this.tail
+        this.tail.next = newNode
+        this.tail = newNode
+        this.length++
+    }
+
+    prepend(value){
+        const newNode = new Node(value)
+        newNode.next = this.head
+        this.head.prev = newNode
+        this.head = newNode
+        this.length++
+    }
+
+    insert(index, value){
+        if(index >= this.length){
+            this.append(value)
+        }
+        const newNode = new Node(value)
+        const leader = this.traverseToIndex(index-1)
+        const follower = leader.next
+        leader.next = newNode;
+        newNode.prev = leader;
+        newNode.next = follower;
+        follower.prev = newNode;
+        this.length++
+    }
+
+    traverseToIndex(index){
+        let counter = 0;
+        let currentNode = this.head;
+        while(counter !== index){
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return currentNode
+    }
+
+    remove(index){
+        const leader = this.traverseToIndex(index-1)
+        const unwantedNode = leader.next
+        leader.next = unwantedNode.next
+        this.length--
+    }
+}
+
+const myLinkedList = new LinkedList(50)
+myLinkedList.prepend(10)
+myLinkedList.append(20)
+myLinkedList.insert(1, 100)
+myLinkedList.printList()
